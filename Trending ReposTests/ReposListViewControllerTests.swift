7308cef6ea0be450557e743148102dba933ba595
O-labeb <85492841+O-labeb@users.noreply.github.com>
2022-12-18 13:52:44 +0200

    add test target with ReposListViewControllerTests

---
+++ Trending ReposTests/ReposListViewControllerTests.swift
@@ -0,0 +1,12 @@
+//
+//  ReposListViewControllerTests.swift
+//  Trending ReposTests
+//
+//  Created by Omar Labib on 18/12/2022.
+//
+
+import XCTest
+@testable import Trending_Repos
+
+final class ReposListViewControllerTests: XCTestCase {
+}
