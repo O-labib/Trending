053dcdaad8df0902458b0b5fb7524ec136b48781
O-labeb <85492841+O-labeb@users.noreply.github.com>
2022-12-19 04:43:11 +0200

    implement shimmering for loading cell

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 target 'Trending Repos' do
   pod 'Kingfisher', '~> 7.0'
   pod 'lottie-ios'
+  pod 'UIView-Shimmer', '~> 1.0'
 
   target 'Trending ReposTests' do
       inherit! :search_paths
--- Trending Repos/Presentation Scenes/Repos List/Cells/Loading Cell/ReposListLoadingCell.swift
+++ Trending Repos/Presentation Scenes/Repos List/Cells/Loading Cell/ReposListLoadingCell.swift
@@ -6,10 +6,31 @@
 //
 
 import UIKit
+import UIView_Shimmer
 
-class ReposListLoadingCell: UITableViewCell {
+class ReposListLoadingCell: UITableViewCell, ShimmeringViewProtocol {
+    @IBOutlet weak var avatarView: UIView!
+    @IBOutlet weak var titleView: UIView!
+    @IBOutlet weak var descriptionView: UIView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         selectionStyle = .none
+        
+        setTemplateWithSubviews(true, viewBackgroundColor: .secondarySystemBackground)
+    }
+    
+    var shimmeringAnimatedItems: [UIView] {
+        [
+            avatarView,
+            titleView,
+            descriptionView
+        ]
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        avatarView.setCircular()
     }
 }
