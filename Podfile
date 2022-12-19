959f71227bbc4e00f567ce4b6afcab73aaad440c
O-labeb <85492841+O-labeb@users.noreply.github.com>
2022-12-19 03:40:22 +0200

    integrate lottie to design failure cell

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 target 'Trending Repos' do
   pod 'Kingfisher', '~> 7.0'
-  
+  pod 'lottie-ios'
+
   target 'Trending ReposTests' do
       inherit! :search_paths
   end
--- Trending Repos/Presentation Scenes/Repos List/Cells/Failure Cell/ReposListFailureCell.swift
+++ Trending Repos/Presentation Scenes/Repos List/Cells/Failure Cell/ReposListFailureCell.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import Lottie
 
 class ReposListFailureCell: UITableViewCell {
+    @IBOutlet weak var animationView: LottieAnimationView!
+    @IBOutlet weak var retryButton: UIButton!
+    
     var retryAction: (() -> Void)?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         selectionStyle = .none
+        
+        animationView.loopMode = .loop
+        animationView.play()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        retryButton.layer.borderWidth = 1
+        retryButton.layer.borderColor = UIColor.systemGreen.cgColor
+    }
+    
+    @IBAction func retryAction(_ sender: Any) {
+        retryAction?()
     }
 }
