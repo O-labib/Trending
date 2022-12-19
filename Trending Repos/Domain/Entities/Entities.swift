b0928a5e7c50fb5a0dc5f8d6ff37f2696280d289
O-labeb <85492841+O-labeb@users.noreply.github.com>
2022-12-19 03:02:27 +0200

    implement url session network client

--- Trending Repos/Domain/Entities/Entities.swift
+++ Trending Repos/Domain/Entities/Entities.swift
@@ -7,23 +7,33 @@
 
 import Foundation
 
-struct Response: Equatable {
+struct Response: Equatable, Decodable {
     let repos: [Repo]
+    
+    enum CodingKeys: String, CodingKey {
+        case repos = "items"
+    }
 }
 
-struct Repo: Equatable {
+struct Repo: Equatable, Codable {
     let owner: Owner
     let name: String
     let description: String
     let language: String
     let stargazersCount: Int
     
-    struct Owner: Equatable {
+    struct Owner: Equatable, Codable {
         let name: String
         let avatarUrl: String
+        
+        
+        enum CodingKeys: String, CodingKey {
+            case name = "login"
+            case avatarUrl
+        }
     }
 }
 
 enum NetworkError: Error, Equatable {
-    case unknown
+    case unknown, invalidStatusCode, invalidURL
 }
--- Trending Repos/Infrastructures/Clients/URLSessionNetworkClient.swift
+++ Trending Repos/Infrastructures/Clients/URLSessionNetworkClient.swift
@@ -8,4 +8,30 @@
 import Foundation
 
 extension URLSession: NetworkClient {
+    func perform<T>(request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) where T : Decodable {
+            dataTask(with: request) { data, response, error in
+                guard error == nil else {
+                    completion(.failure(error!))
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse,
+                    httpResponse.statusCode == 200 else {
+                    return completion(.failure(NetworkError.invalidStatusCode))
+                }
+                
+                guard let data else {
+                    return completion(.failure(NetworkError.unknown))
+                }
+
+                do {
+                    let decoded = JSONDecoder()
+                    decoded.keyDecodingStrategy = .convertFromSnakeCase
+                    let model = try decoded.decode(T.self, from: data)
+                    completion(.success(model))
+                } catch let error {
+                    completion(.failure(error))
+                }
+            }.resume()
+    }
 }
