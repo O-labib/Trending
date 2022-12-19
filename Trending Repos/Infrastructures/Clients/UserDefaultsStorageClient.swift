758bcd12fe1f8f6e28845641904d31dedc0f3569
O-labeb <85492841+O-labeb@users.noreply.github.com>
2022-12-19 02:52:24 +0200

    implement UserDefaults local storage client

--- Trending Repos/Infrastructures/Clients/UserDefaultsStorageClient.swift
+++ Trending Repos/Infrastructures/Clients/UserDefaultsStorageClient.swift
@@ -8,4 +8,22 @@
 import Foundation
 
 extension UserDefaults: LocalStorageClient {
+    enum Keys {
+        static let repos = "reposKey"
+    }
+    func store<T>(_ value: T) throws where T : Encodable {
+        let data = try JSONEncoder().encode(value)
+        setValue(data, forKey: Keys.repos)
+    }
+    
+    func fetch<T>() throws -> T where T : Decodable {
+        guard let data = data(forKey: Keys.repos) else {
+            throw LocalStorageError.invalidStoredData
+        }
+        return try JSONDecoder().decode(T.self, from: data)
+    }
+}
+
+enum LocalStorageError: Error {
+    case invalidStoredData
 }
